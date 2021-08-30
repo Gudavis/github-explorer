2ea6b5481b254667caff56f7c82604661d73b49a
Gustavo Davis <gustavo.davis@outlook.com>
2021-08-30 16:10:14 -0300

    add hooks to list github repos

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,10 @@
+export function RepositoryItem(props) {
+  return (
+    <li>
+      <h1>{props.repository.name}</h1>
+      <p>{props.repository.description}</p>
+
+      <a href={props.repository.html_url}>Acessar o repositório</a>
+    </li>
+  )
+}
